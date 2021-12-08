bed673216fccf70d9dee317cfb6cb3e2ce74de7a
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 <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>
2021-12-07 22:02:16 -0300

    --

---
+++ projetoRestaurante/controle/TipoDB.cs
@@ -0,0 +1,45 @@
+﻿using prjRestaurante.controle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoRestaurante.controle
+{
+    class TipoDB
+    {
+        string con = Conexao.Open("localhost", "restaurantedb", "root", "minas");
+        public object selectAll()
+        {
+            using (var banco = new modelo.restaurantedbEntidades())
+            {
+                banco.Database.Connection.ConnectionString = con;
+                var query = from linhas in banco.tipo orderby linhas.idtipo select linhas;
+
+                return query.ToList();
+            }
+        }
+
+        public void inserir(modelo.tipo reg)
+        {
+            using (var banco = new modelo.restaurantedbEntidades())
+            {
+                banco.Database.Connection.ConnectionString = con;
+                banco.tipo.Add(reg);
+                banco.SaveChanges();
+            }
+        }
+
+        public void editar(modelo.tipo reg)
+        {
+            using (var banco = new modelo.restaurantedbEntidades())
+            {
+                banco.Database.Connection.ConnectionString = con;
+                modelo.tipo Velho = banco.tipo.First(i => i.idtipo == reg.idtipo);
+                banco.Entry(Velho).CurrentValues.SetValues(reg);
+                banco.SaveChanges();
+            }
+        }
+    }
+}
--- projetoRestaurante/visao/FormFichaProduto.Designer.cs
+++ projetoRestaurante/visao/FormFichaProduto.Designer.cs
@@ -205,6 +205,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
             this.ClientSize = new System.Drawing.Size(740, 510);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
